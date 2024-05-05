ae37961e9942447d79b14a5313c3e8a9d6b89d25
Rajan <rajankumar656@gmail.com>
2024-05-05 17:56:39 -0400

    test

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using AcademyAPI.Model;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using Microsoft.EntityFrameworkCore;
 
 namespace AcademyAPI.Data
@@ -9,6 +10,24 @@ namespace AcademyAPI.Data
         { 
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order_Customer>().HasKey(oc => new
+            {
+                //jjj
+                oc.OrderId,
+                oc.CustomerId
+            });
+
+            modelBuilder.Entity<Order_Customer>().HasOne(o => o.Order).WithMany(oc => oc.Orders_Customers).HasForeignKey(oc => oc.OrderId);
+            modelBuilder.Entity<Order_Customer>().HasOne(c => c.Customer).WithMany(oc => oc.Orders_Customers).HasForeignKey(oc => oc.CustomerId);
+            
+            base.OnModelCreating(modelBuilder);
+        }
         public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Order_Customer> Orders_Customers { get; set; }
     }
 }
